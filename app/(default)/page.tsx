b76e4bdcd1da6fa789e7f1dc9d37d08d32dc8ab1
Nacho Lacruz <hola@lacruzwebdev.com>
2023-12-16 23:02:01 +0100

    Category filter

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,8 +1,5 @@
-import Card from '@/components/ui/card'
-import CardTextSkeleton from '@/components/ui/card-text-skeleton'
-import Grid from '@/components/ui/grid'
-import { fetchAPI, fetchTodayEvents } from '@/lib/api'
-import { Suspense } from 'react'
+import Eventos from '@/components/eventos'
+import { fetchTodayEvents } from '@/lib/api'
 
 type Props = {}
 export default async function Home({}: Props) {
@@ -12,24 +9,11 @@ export default async function Home({}: Props) {
     day: 'numeric'
   } as const
   const date = new Intl.DateTimeFormat('es-ES', options).format(new Date())
-  return (
-    <section className="max-w-screen-xl mx-auto p-8">
-      <h1 className="acc-h">Imperio Español</h1>
-      <p className="text-3xl mb-2">Tal día como hoy...</p>
-      <p className="text-primary text-xl mb-4">{date}</p>
-      <Grid>
-        {data.map((evento: Evento, index: number) => (
-          <Card
-            key={evento.id}
-            id={evento.id}
-            fecha={evento.Fecha}
-            title={evento.Titulo}
-            description={evento.Descripcion}
-            img={evento.Imagen[0]}
-            relevancia={evento.Relevancia}
-          />
-        ))}
-      </Grid>
-    </section>
-  )
+  let categorias: any[] = ['Todas']
+  data.forEach((item: any) => {
+    item.Categorias.forEach((cat: any) => {
+      categorias.push(cat.Nombre)
+    })
+  })
+  return <Eventos data={data} date={date} categorias={categorias} />
 }
---
+++ components/eventos.tsx
@@ -0,0 +1,54 @@
+'use client'
+import { useState } from 'react'
+import Card from './ui/card'
+import { Categorias } from './ui/categorias'
+import Grid from './ui/grid'
+import { hasCategory } from '@/lib/helpers'
+
+type Props = {
+  data: any
+  date: any
+  categorias: any
+}
+export default function Eventos({ data, date, categorias }: Props) {
+  const [activeEvents, setActiveEvents] = useState(data)
+  const [activeCategory, setActiveCategory] = useState('Todas')
+
+  function onCategoryChange() {}
+
+  return (
+    <section className="max-w-screen-xl mx-auto p-8">
+      <h1 className="acc-h">Imperio Español</h1>
+      <div className="flex justify-between">
+        <div>
+          <p className="text-3xl mb-2">Tal día como hoy...</p>
+          <p className="text-primary text-xl mb-4">{date}</p>
+        </div>
+        <Categorias
+          opciones={categorias}
+          active={{ activeCategory, setActiveCategory }}
+        />
+      </div>
+      <Grid>
+        {activeEvents.map((evento: Evento, index: number) => {
+          if (
+            hasCategory(evento, activeCategory) ||
+            activeCategory === 'Todas'
+          ) {
+            return (
+              <Card
+                key={evento.id}
+                id={evento.id}
+                fecha={evento.Fecha}
+                title={evento.Titulo}
+                description={evento.Descripcion}
+                img={evento.Imagen[0]}
+                relevancia={evento.Relevancia}
+              />
+            )
+          }
+        })}
+      </Grid>
+    </section>
+  )
+}
