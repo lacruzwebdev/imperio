7fe06a6b569a9f72e59ef547df321f6a2afbd5ab
Nacho Lacruz <hola@lacruzwebdev.com>
2023-12-16 22:59:22 +0100

    Categories list and date formatting

--- app/(default)/[eventId]/page.tsx
+++ app/(default)/[eventId]/page.tsx
@@ -30,6 +30,7 @@ export default async function Evento({ params }: Props) {
     .format(eventDate)
     .split(' ')
     .slice(0, -2)
+    .filter((item) => item !== 'de')
     .join(' ')
   const year = new Intl.DateTimeFormat('es-ES', { year: 'numeric' }).format(
     eventDate
@@ -43,11 +44,9 @@ export default async function Evento({ params }: Props) {
         <p className="text-center mb-2 text-xl text-primary font-bold tracking-tight">
           {year}
         </p>
-        <p className="text-center mb-2 text-xl text-primary font-bold tracking-tight">
-          {date}
-        </p>
-        <p className="text-center mb-2 text-xl text-primary font-bold tracking-tight">
-          {day}
+        <p className="text-center mb-2 text-xl tracking-tight">{date}</p>
+        <p className="text-center mb-2 text-xl tracking-tight">
+          {`${day[0].toUpperCase()}${day.substring(1)}`}
         </p>
       </div>
       {img && (
@@ -59,16 +58,18 @@ export default async function Evento({ params }: Props) {
           alt={img[0].alternativeText}
         />
       )}
-      <h1 className="mt-8 text-2xl font-bold tracking-tight">{event.Titulo}</h1>
+      <h1 className="mt-8 mb-4 text-2xl font-bold tracking-tight">
+        {event.Titulo}
+      </h1>
       <p
         dangerouslySetInnerHTML={{
           __html: event.Descripcion.replace(/\n/g, '<br />')
         }}
       ></p>
-      <div className="mt-2 mb-12 flex items-center gap-2">
+      <div className="mt-2 mb-2 flex items-center gap-2">
         <p>
           Autor:{' '}
-          <a className="hover:text-primary" href={event.Autores.Web}>
+          <a className="hover:text-primary font-bold" href={event.Autores.Web}>
             {event.Autores.Nombre}
           </a>
         </p>
@@ -82,6 +83,17 @@ export default async function Evento({ params }: Props) {
             <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
           </svg>
         </a>
+      </div>
+      <div className="mt-2 mb-12">
+        <p className="text-primary">
+          Categorías:{' '}
+          {event.Categorias.map((cat: any, index: number, arr: any[]) => (
+            <span key={cat.id}>
+              {cat.Nombre}
+              {index < arr.length - 1 ? ', ' : ''}
+            </span>
+          ))}
+        </p>
       </div>
       {event.Imagen.length > 1 && <Carousel slides={event.Imagen} />}
       {event.Latitud !== 0 && event.Longitud !== 0 && (
