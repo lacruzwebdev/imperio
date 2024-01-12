7c4ca4c2b8808277d14fb748a891d0f8e4d93368
Nacho Lacruz <hola@lacruzwebdev.com>
2024-01-12 09:44:33 +0100

    Removed console.log

--- app/(default)/lecturas/page.tsx
+++ app/(default)/lecturas/page.tsx
@@ -3,11 +3,9 @@ import { fetchLecturas } from "@/lib/api"
 import { getCategories } from "@/lib/helpers";
 
 export default async function Hitos() {
-   const data = await fetchLecturas();
-
-   const categorias = getCategories(data);
-   console.log(categorias)
-   
+  const data = await fetchLecturas();
+  const categorias = getCategories(data);
+  
   return (
     <div>
       <Lecturas data={data} categorias={categorias}/>
