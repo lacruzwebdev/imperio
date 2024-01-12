3ad3275173ba18b0783f87e508791eaf0620b529
Nacho Lacruz <hola@lacruzwebdev.com>
2024-01-12 09:44:52 +0100

    Skip to content

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
+import SkipToContent from '@/components/ui/skip-to-content'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>
+        <SkipToContent />
         <Header />
         {children}
         <Footer />
---
+++ components/ui/skip-to-content.tsx
@@ -0,0 +1,10 @@
+export default function SkipToContent() {
+  return (
+      <a
+      href="#main"
+      className="transition z-10 text-white left-0 bg-primary text-primary-content absolute p-3 m-3 -translate-y-16 focus:translate-y-0"
+      >
+        Saltar a contenido
+      </a>
+  )
+}
