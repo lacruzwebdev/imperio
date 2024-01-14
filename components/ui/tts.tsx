295ef5fd497bf8d756a04c1dde20e9619c2883ce
Nacho Lacruz <hola@lacruzwebdev.com>
2024-01-14 10:46:33 +0100

    TTS icon change

--- components/ui/tts.tsx
+++ components/ui/tts.tsx
@@ -7,9 +7,11 @@ interface Props extends ComponentPropsWithoutRef<'button'> {
 }
 
 export default function TTS({ text, ...rest }: Props) {
+  const [isClicked, setIsClicked] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
 
   const speak = async () => {
+    setIsClicked(true);
     if (text.length <= 0) return;
     if (!isPlaying) {
       await new Promise((resolve, reject) => {
@@ -27,7 +29,7 @@ export default function TTS({ text, ...rest }: Props) {
 
   return (
     <button onClick={speak} {...rest}>
-      {isPlaying ?
+      {isPlaying || !isClicked ?
         <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -44,7 +46,7 @@ export default function TTS({ text, ...rest }: Props) {
           <path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path>
         </svg> :
         <svg 
-        xmlns="http://www.w3.org/2000/svg"
+        xmlns="http://www.w3.org/2000/svg" 
         width="24" 
         height="24" 
         viewBox="0 0 24 24" 
@@ -53,8 +55,10 @@ export default function TTS({ text, ...rest }: Props) {
         strokeWidth="2" 
         strokeLinecap="round" 
         strokeLinejoin="round" 
-        className="feather feather-volume">
+        className="feather feather-volume-x">
           <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
+          <line x1="23" y1="9" x2="17" y2="15"></line>
+          <line x1="17" y1="9" x2="23" y2="15"></line>
         </svg>
       }
     </button>
