384787c95712b0f956cf4b0e36269fd95d935750
Nacho Lacruz <hola@lacruzwebdev.com>
2024-02-05 11:25:31 +0100

    Revert "Map size and language"
    This reverts commit 5a3520482bd7c0df0df9c5e40007ba759dd0d6a6.

--- components/eventos.tsx
+++ components/eventos.tsx
@@ -41,4 +41,66 @@ export default function Eventos({ data, date, categorias }: Props) {
     setActiveTab(`${e.currentTarget.textContent.toLowerCase()}`)
   }
 
-  const posiciones = data.map(item => ({lat: item.Latitud, long: item.Longitud}))  const hasMap = posiciones.some(item => item.lat !== 0 || item.long !== 0)    return (    <section className="max-w-screen-xl mx-auto p-8">      <h1 className="acc-h">Imperio Español</h1>      <Tabs defaultValue='eventos' value={activeTab} className="grid place-items-center">        <TabsList>          <TabsTrigger onClick={handleTab} value="eventos" className="data-[state=active]:bg-red-500 data-[state=active]:text-white">Eventos</TabsTrigger>          <TabsTrigger onClick={handleTab} value="mapa" className="data-[state=active]:bg-red-500 data-[state=active]:text-white">Mapa</TabsTrigger>        </TabsList>        {Array.isArray(data) && data.length > 0 ?        <>      <TabsContent className="w-full" value="eventos">      <div className="flex justify-between flex-col md:flex-row mb-8 md:mb-0">        <div>          <Link href="/">            <h2 className="text-3xl mb-2">Tal día como hoy...</h2>          </Link>          <p className="text-primary text-xl mb-4">{date}</p>        </div>        <Categorias          opciones={categorias}          active={{ activeCategory, setActiveCategory }}        />      </div>      <Grid>        {activeEvents.map((evento: Evento, index: number) => {          if (            hasCategory(evento, activeCategory) ||            activeCategory === 'Todas'            ) {              return (                <Card                type={'eventos'}                key={evento.id}                id={evento.id}                fecha={evento.Fecha}                title={evento.Titulo}                description={evento.Descripcion}                img={evento.Imagen[0]}                relevancia={evento.Relevancia}                priority={index < 2}                />                )              }            })}      </Grid>       </TabsContent>      <TabsContent className="w-full mapa-eventos" value="mapa">        {hasMap ?          <EventoMap positions={data} zoom={2} height={100}/>          :  <p className="text-xl mt-8">No hay eventos con localización ¡Vuelve mañana para descubrir más!</p>        }      </TabsContent>      </>      : <p className="text-xl mt-8">No hay eventos ¡Vuelve mañana para descubrir más!</p>  }      </Tabs>    </section>    )  }  
+  const posiciones = data.map(item => ({lat: item.Latitud, long: item.Longitud}))
+  const hasMap = posiciones.some(item => item.lat !== 0 || item.long !== 0)
+  
+  return (
+    <section className="max-w-screen-xl mx-auto p-8">
+      <h1 className="acc-h">Imperio Español</h1>
+      <Tabs defaultValue='eventos' value={activeTab} className="grid place-items-center">
+        <TabsList>
+          <TabsTrigger onClick={handleTab} value="eventos" className="data-[state=active]:bg-red-500 data-[state=active]:text-white">Eventos</TabsTrigger>
+          <TabsTrigger onClick={handleTab} value="mapa" className="data-[state=active]:bg-red-500 data-[state=active]:text-white">Mapa</TabsTrigger>
+        </TabsList>
+        {Array.isArray(data) && data.length > 0 ?
+        <>
+      <TabsContent className="w-full" value="eventos">
+      <div className="flex justify-between flex-col md:flex-row mb-8 md:mb-0">
+        <div>
+          <Link href="/">
+            <h2 className="text-3xl mb-2">Tal día como hoy...</h2>
+          </Link>
+          <p className="text-primary text-xl mb-4">{date}</p>
+        </div>
+        <Categorias
+          opciones={categorias}
+          active={{ activeCategory, setActiveCategory }}
+        />
+      </div>
+      <Grid>
+        {activeEvents.map((evento: Evento, index: number) => {
+          if (
+            hasCategory(evento, activeCategory) ||
+            activeCategory === 'Todas'
+            ) {
+              return (
+                <Card
+                type={'eventos'}
+                key={evento.id}
+                id={evento.id}
+                fecha={evento.Fecha}
+                title={evento.Titulo}
+                description={evento.Descripcion}
+                img={evento.Imagen[0]}
+                relevancia={evento.Relevancia}
+                priority={index < 2}
+                />
+                )
+              }
+            })}
+      </Grid> 
+      </TabsContent>
+      <TabsContent className="w-full mapa-eventos" value="mapa">
+        {hasMap ?
+          <EventoMap positions={data} zoom={2} height={100}/>
+          :  <p className="text-xl mt-8">No hay eventos con localización ¡Vuelve mañana para descubrir más!</p>
+        }
+      </TabsContent>
+      </>
+      : <p className="text-xl mt-8">No hay eventos ¡Vuelve mañana para descubrir más!</p>
+  }
+      </Tabs>
+    </section>
+    )
+  }
+  
