0b835a5b95385ec60e00a549e25374cca311cb2f
Ignacio Lacruz <hola@lacruzwebdev.com>
2025-05-13 10:04:55 +0200

    Remove grid gap in mobile

--- components/ui/grid.tsx
+++ components/ui/grid.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 export default function Grid({ cols, rows, children }: Props) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-8">
       {children}
     </div>
   )
